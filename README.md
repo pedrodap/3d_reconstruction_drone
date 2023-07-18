1de4f9ac07e6527be55a9b6e92376d8e457adada
Pedro <pedroa7a17@gmail.com>
2023-07-18 22:30:21 +0100

    [Maint]:general cleanup

--- README.md
+++ README.md
@@ -1,17 +1,6 @@
 # 3d_reconstruction_drone
 
-1. Prerequisites
-
-Install prerequisites of https://github.com/HKUST-Aerial-Robotics/A-LOAM
-
-Make aloam work on 20.04
-Step 1: Configure the headers to use
-#include <opencv2/opencv.hpp> instead of #include <opencv/cv.h>
-
-Step 2: Configure CMakeLists.txt to use
-set(CMAKE_CXX_FLAGS "-std=c++14") instead of set(CMAKE_CXX_FLAGS "-std=c++11")
-
-2. Build 3d_reconstruction_drone
+1. Build 3d_reconstruction_drone
 
 cd ~/catkin_ws/src
 git clone
@@ -19,20 +8,12 @@ cd ../
 catkin_make
 source ~/catkin_ws/devel/setup.bash
 
-3. Run 3d_reconstruction_drone
+2. Run 3d_reconstruction_drone
 
-roslaunch sjtu_drone simple.launch
-
-Another tab
-
-roslaunch aloam_velodyne aloam_velodyne_HDL_32.launch
+roslaunch sjtu-drone project.launch
 
 Another tab
 
 rosrun sjtu_drone drone_waypoints
 
-
-
-
-# Aloam
 
--- reconstruct/src/reconstruct_node.cpp
+++ reconstruct/src/reconstruct_node.cpp
@@ -53,12 +53,11 @@ void syncAndTfPointCloud::transformPointCloud::callback(const sensor_msgs::Point
 
    countPoint++;
    if(countPoint == 50){
-        /* TODO: sub sumcloud with normal cloud and then when feature is proven eliminate this*/
         std::string filename = "/home/alphad/catkin_ws/src/cloud_" + std::to_string(odometryALOAM->header.stamp.sec) + ".pcd";
         pcl::io::savePCDFileASCII (filename, *sumCloud);
         ROS_INFO("PointCloud Written");
         countPoint = 0;
-          // Create a KD-Tree
+        // Create a KD-Tree
         pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
         // Init object (second point type is for the normals, even if unused)
         pcl::MovingLeastSquares<pcl::PointXYZ, pcl::PointNormal> mls;
@@ -87,7 +86,7 @@ void syncAndTfPointCloud::transformPointCloud::callback(const sensor_msgs::Point
         std::string meshfilename = "/home/alphad/catkin_ws/src/mesh_" + std::to_string(odometryALOAM->header.stamp.sec) + ".vtk";
         pcl::io::saveVTKFile(meshfilename, mesh_poisson);
         ROS_INFO("Mesh created"); 
-        //reset sumCloud to last value, will implement concatenation of meshes once end product is good
+        //reset sumCloud to last value
         *sumCloud = *cloudFilter;
    }
 }
